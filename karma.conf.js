c487562ab67494022941efdbf5885b3fe6b29abd
Mike Fikes <mike@fikesfarm.com>
2019-01-26 09:03:28 -0500

    Add Travis config

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,19 @@ module.exports = function (config) {
   var root = 'run/compiled/karma/test' // same as :output-dir
   var junitOutputDir = process.env.CIRCLE_TEST_REPORTS || "run/compiled/karma/test/junit"
 
+    config.set({
+        frameworks: ['cljs-test'],
+        browsers: ['PhantomJS'],
+        files: [
+            root + '/../test.js', // same as :output-to
+            {pattern: root + '/../test.js.map', included: false, watched: false},
+            {pattern: root + '/**/*.+(cljs|cljc|clj|js|js.map)', included: false, watched: false}
+        ],
+
+        client: {
+            args: ['re_frame.test_runner.run_karma']
+        },
+
   config.set({
     frameworks: ['cljs-test'],
     browsers: ['ChromeHeadless'],
